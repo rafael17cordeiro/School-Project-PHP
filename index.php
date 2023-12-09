7b7234bc67775d05fd1059524c4f36f3894b5138
rafael17cordeiro <59150464+rafael17cordeiro@users.noreply.github.com>
2023-12-09 18:36:07 +0000

    commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <a href="index.php"><img class="logo" src="img/logo.png" alt=""></a>
+        <input type="checkbox" id="check">
+        <label for="check" class="icons">
+            <i class='bx bx-menu' id="menu-icon"></i>
+            <i class="bx bx-x" id="close-icon"></i>
+        </label>
+        <nav class="navigation">
+            <a href="home.html">Therapists</a>
+            <a href="about.html">Contact</a>
+            <a class="loginBtn" href=""><button>Login</button></a>
+        </nav>
+    </header>
+
+    <div class="welcome-section">
+        <div class="welcome-content">
+            <h1>Our legacy speaks through our healed clients</h1>
+            <a class="moreBtn" href="#sessions"><button>More About it</button></a>
+        </div>
+    </div>
+
+
+
+    <div class="sessions" id="sessions">
+        <div class="sessions-content">
+            <h1 class="titulo">Popular Therapy Sessions</h1>
+                <div class="cards">
+                    <div class="carta">
+                        <h1>60$/session</h1>
+                        <h5>Therapist Details:</h5><br>
+                        <p>Jackie Chan <br>5 years of experience</p>
+                        <a class="moreBtn" href=""><button>View Therapist</button></a>
+                    </div>
+                    <div class="carta-bigger">
+                        <h1>90$/session</h1>
+                        <h5>Therapist Details:</h5><br>
+                        <p>Joaquin Phoenix <br>15 years of experience</p>
+                       
+                        <a class="moreBtn" href=""><button>View Therapist</button></a>
+                    </div>
+                    <div class="carta">
+                        <h1>40$/session</h1>
+                        <h5>Therapist Details:</h5><br>
+                        <p>Ross Geller <br>1 years of experience</p>
+                   
+                        <a class="moreBtn" href=""><button>View Therapist</button></a>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+    <div class="depoimentos">
+    <div class="imagem">
+       <img src="img/hero.jpg" alt="">
+    </div>
+    <div class="texto">
+       <h1>MyMind Can Help You</h1><br>  
+       <p>MyMind can help anyone that struggles with mental health problems with guided therapy sessions.</p>
+       <p>Although therapy in Canada is available, it is not cheap and fees can range from $50-$240 per session. Many people with mental health problems require 12-16 sessions on average and only 50 percent of patients recover as indicated by self-reported symptom measures.</p>
+    </div>
+    
+</div>
+<footer>
+        <div class="footer-content">
+            <p>&copy; 2023 MyMind. All rights reserved.</p>
+        </div>
+    </footer>
+
+
+</body>
+</html>
